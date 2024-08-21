e34091257e813982bf9aab36aa10aec6d7b8cf9a
Ante Dražić <96959476+readycool@users.noreply.github.com>
2024-08-22 00:32:03 +0200

    v1.1.25

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  url64bit       = 'https://github.com/oven-sh/bun/releases/download/bun-v1.1.24/bun-windows-x64.zip'
-  checksum64     = '3ad39f36b1fff296cb794d96e03f4880a8e07bbd5256437931555e1c7b590b3a8767b7765f84fc0ae0e9aa9e5543fd8a8ef901c855331fda5d1888836d0731f8'
+  url64bit       = 'https://github.com/oven-sh/bun/releases/download/bun-v1.1.25/bun-windows-x64.zip'
+  checksum64     = 'd56ef849f4eee18b77a035e240e620eca006b837a492fa06a5db1653fc825c01cf7bb7ddd123e787bd62e427772dd9e951b05fa30d5975d9342b5c8051344479'
   checksumType64 = 'sha512'
 }
 
