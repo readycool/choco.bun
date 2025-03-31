d80c9471a6aa2e2110930edb285ad91313a83665
Ante Dražić <96959476+readycool@users.noreply.github.com>
2025-03-31 22:34:38 +0200

    v1.2.7

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,12 +9,12 @@ $packageArgs = @{
 }
 
 if ($avx2) {
-  $packageArgs.url64bit       = 'https://github.com/oven-sh/bun/releases/download/bun-v1.2.6/bun-windows-x64.zip'
-  $packageArgs.checksum64     = '7fe78a7d8b213ff7eac10ef7474ee606d4c51c35986db4c428acd5ad151a7b77'
+  $packageArgs.url64bit       = 'https://github.com/oven-sh/bun/releases/download/bun-v1.2.7/bun-windows-x64.zip'
+  $packageArgs.checksum64     = 'f8596731041e804131096f4b4fa71236521bb9344fb1f2538d5f8e08e3c3e250'
   $packageArgs.checksumType64 = 'sha256'
 } else {
-  $packageArgs.url64bit       = 'https://github.com/oven-sh/bun/releases/download/bun-v1.2.6/bun-windows-x64-baseline.zip'
-  $packageArgs.checksum64     = '51a3c6f8fcd0fcfc75f3af22177d8774197cbc60bc228344f1f0434446983be0'
+  $packageArgs.url64bit       = 'https://github.com/oven-sh/bun/releases/download/bun-v1.2.7/bun-windows-x64-baseline.zip'
+  $packageArgs.checksum64     = 'c9e15145372dceaa3d5fa79d12b3d47ecf63e144a2137d725b3d358094b0cf73'
   $packageArgs.checksumType64 = 'sha256'
 }
 
